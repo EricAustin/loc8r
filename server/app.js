3ba6b3c59103b5cd7771326420ae43e4ac110ea6
Eric Austin <ewaustin@gmail.com>
2017-09-05 21:45:32 -0500

    basic static server added

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(express.static('server/public'));
+
+app.use(express.static('public'));
+
+app.listen(port, function() {
+  console.log('localhost running on port', port);
+});
