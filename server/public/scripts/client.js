ffc2f79c163214e58922ff864a26289258014708
Eric Austin <ewaustin@gmail.com>
2017-09-14 11:03:43 -0500

    are my scripts secure now?

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,15 @@
 var myApp = angular.module('myApp', ['ngMap', 'ngRoute']);
 
-myApp.config(function ($routeProvider) {
-  $routeProvider.when("/", {
-    templateUrl: "views/main.html",
-    // controller: 'MapController as mc'
 
-  })
-    .when("/map", {
-      templateUrl: "/views/map.html",
-      controller: 'MapController as mc'
-    })
 
-});
+myApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/', {
+      templateUrl: 'views/main.html'
+  }).when('/map', {
+      templateUrl: 'views/map.html',
+      controller: 'MapController',
+      controllerAs: 'mc'
+  });
+
+
+}]) //end app config
