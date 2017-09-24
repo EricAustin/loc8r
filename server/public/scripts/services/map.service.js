2900e7f739284f3da8469d1522496d7e835f0b46
Eric Austin <ewaustin@gmail.com>
2017-09-23 21:59:21 -0500

    code comments and console.logs commented out

--- server/public/scripts/services/map.service.js
+++ server/public/scripts/services/map.service.js
@@ -5,54 +5,46 @@ myApp.service('MapService', ['$http', 'NgMap', function ($http, NgMap) {
   var ID;
   var bounds = new google.maps.LatLngBounds();
   var newPin = {};
-
-
-
-
   self.locations = { list: [] };
 
   self.getLocations = function () {
     $http.get('/pins/' + self.newPin.group).then(function (response) {
-      console.log('self.newPin.group is', self.newPin.group);
-
+      // console.log('self.newPin.group is', self.newPin.group);
       self.locations.list = response.data;
       // console.log('get response: ', self.locations);
       self.drawMap();
     })
-  };
-
+  }; // end self.getLocations
 
   self.addPin = function (newPin) {
-    console.log('going to send this object to the server: ', newPin);
+    // console.log('going to send this object to the server: ', newPin);
     $http.post('/pins', newPin).then(function (response) {
       self.newPin = newPin
-
-      console.log('service post response.data: ', response.data);
+      // console.log('service post response.data: ', response.data);
       ID = response.data
       // console.log('self.newID is', self.newID);
       // console.log('MapService.newID is', MapService.newID);
       self.getLocations();
     });
-  };
+  }; // end self.addPin
 
   self.updatePin = function (changePin) {
     changePin.ID = ID;
-    console.log('updating pin with ID:', changePin.ID);
-    console.log('new location is:', changePin.location);
-    console.log('new timestamp is', changePin.timestamp);
-    
+    // console.log('updating pin with ID:', changePin.ID);
+    // console.log('new location is:', changePin.location);
+    // console.log('new timestamp is', changePin.timestamp);
     $http.put('/pins', changePin).then(function () {
       self.getLocations();
     })
-  }
+  } // end self.updatePin
 
   self.deletePin = function () {
-    console.log('service to delete id: ', ID);
+    // console.log('service to delete id: ', ID);
     $http.delete('/pins/' + ID).then(function (response) {
-      console.log('service delete response:', response);
+      // console.log('service delete response:', response);
       self.getLocations();
     });
-  }
+  } // end self.deletePin
 
 
   self.drawMap = function () {
@@ -62,26 +54,26 @@ myApp.service('MapService', ['$http', 'NgMap', function ($http, NgMap) {
     // console.log('self.locations.list is ', self.locations.list);
     // console.log('self.locations.list.length is ', self.locations.list.length);
 
-    if (self.locations.list.length == 0) {
-      // console.log('if hit');
-      NgMap.getMap().then(function (map) {
-        map.setCenter({ lat: 47.115567, lng: -101.299663 });
-        map.setZoom(4);
-      })
-    } else {
-      // console.log('else hit');
-      for (var i = 0; i < self.locations.list.length; i++) {
-        var latlng = new google.maps.LatLng(self.locations.list[i].location[0], self.locations.list[i].location[1]);
-        bounds.extend(latlng);
-        // console.log(MapService.locations[i].location[0]);
-        // console.log(MapService.locations[i].location[1]);
-      }
-      NgMap.getMap().then(function (map) {
-        map.setCenter(bounds.getCenter());
-        map.fitBounds(bounds);
-      });
+    // if (self.locations.list.length == 0) {
+    //   // console.log('if hit');
+    //   NgMap.getMap().then(function (map) {
+    //     map.setCenter({ lat: 47.115567, lng: -101.299663 });
+    //     map.setZoom(4);
+    //   })
+    // } else {
+    // console.log('else hit');
+    for (var i = 0; i < self.locations.list.length; i++) {
+      var latlng = new google.maps.LatLng(self.locations.list[i].location[0], self.locations.list[i].location[1]);
+      bounds.extend(latlng);
+      // console.log(MapService.locations[i].location[0]);
+      // console.log(MapService.locations[i].location[1]);
     }
-  }
+    NgMap.getMap().then(function (map) {
+      map.setCenter(bounds.getCenter());
+      map.fitBounds(bounds);
+    });
+    // }
+  } // end self.drawMap
 
 
 
--- server/routes/pins.js
+++ server/routes/pins.js
@@ -6,84 +6,70 @@ var pin = require('../models/document.model.js');
 // router.get('/:group', function (req, res) {
 router.get('/:group', function (req, res) {
     // find all of the people in the collection
-    console.log('finding pins with group:', req.params.group);
-
+    // console.log('finding pins with group:', req.params.group);
     pin.find({ 'group': req.params.group }, function (err, data) {
-
-
         if (err) {
-            console.log('find error: ', err);
+            // console.log('find error: ', err);
             res.sendStatus(500);
         } else {
-            console.log('found data: ', data);
+            // console.log('found data: ', data);
             res.send(data);
         }
     });
-});
+}); // end router.get
 
 router.post('/', function (req, res) {
-    console.log('new pin to store: ', req.body);
-
-
+    // console.log('new pin to store: ', req.body);
     // use model/constructor to make a Mongoose Object
     var newPin = new pin(req.body);
-
     // insert into our collection
     newPin.save(function (err, data) {
-        console.log('saved to the collection: ', data);
+        // console.log('saved to the collection: ', data);
         if (err) {
-            console.log('save error: ', err);
-
+            // console.log('save error: ', err);
             res.sendStatus(500);
         } else {
             res.send(data._id);
         }
-
     });
-});
+}); // end router.post
+
 router.put('/', function (req, res) {
     var changePin = new pin;
     changePin.ID = req.body.ID;
     changePin.location = req.body.location;
     changePin.timestamp = req.body.timestamp;
-    console.log('pins.js line 47 changePin.ID is:', changePin.ID);
-    console.log('pins.js line 47 changePin.location is:', changePin.location);
-    console.log('pins.js line 47 changePIn.timestamp is:', changePin.timestamp);
-    
-
+    // console.log('pins.js line 47 changePin.ID is:', changePin.ID);
+    // console.log('pins.js line 47 changePin.location is:', changePin.location);
+    // console.log('pins.js line 47 changePIn.timestamp is:', changePin.timestamp);
     pin.findByIdAndUpdate(
         { _id: changePin.ID },
-        {
-            $set: { location: changePin.location, timestamp: changePin.timestamp }
-        },
+        { $set: { location: changePin.location, timestamp: changePin.timestamp } },
         function (err, data) {
             if (err) {
-                console.log('save error: ', err);
-
-                res.sendStatus(500);
-            } else {
-                res.sendStatus(200);
-            }
-
-        });
-});
-
-router.delete('/:id', function (req, res) {
-    console.log('deleting pin with id:', req.params.id);
-
-    pin.findByIdAndRemove(
-        { _id: req.params.id },
-        function (err, data) {
-            if (err) {
-                console.log('delete error: ', err);
-
+                // console.log('save error: ', err);
                 res.sendStatus(500);
             } else {
                 res.sendStatus(200);
             }
         }
     );
-});
+}); // end router.put
+
+router.delete('/:id', function (req, res) {
+    // console.log('deleting pin with id:', req.params.id);
+    pin.findByIdAndRemove(
+        { _id: req.params.id },
+        function (err, data) {
+            if (err) {
+                // console.log('delete error: ', err);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    );
+}); // end router.delete
 
 
 module.exports = router;
