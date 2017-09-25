1a3371ba7c7c3b16d32097a05a2e2b8ec90fbf7a
Eric Austin <ewaustin@gmail.com>
2017-09-24 23:00:03 -0500

    timing finally right

--- server/public/scripts/controllers/map.controller.js
+++ server/public/scripts/controllers/map.controller.js
@@ -1,143 +1,132 @@
 
 myApp.controller('MapController', ['MapService', 'NgMap', '$location', function (MapService, NgMap, $location) {
+  console.log('enter controller');
   // console.log('Map Controller loaded.');
 
+  var launched;
+  var timer
+  var test = 0
   var self = this;
   var userID = MapService.newID
   var newPin = {};
   var changePin = {};
-  var timer;
   var refreshrate = 10000;
   var options = {
     enableHighAccuracy: true,
-    timeout: 5000,
+    timeout: 7000,
     maximumAge: 0
   };
 
-  function setTimer() {
-    timer = setTimeout(function () {
-      setTimeout(function () {
-        self.updateLocation()
-        console.log('timeout hit');
-      }, refreshrate);
-    })
-  }
 
-  function error(err) {
-    // console.warn(`ERROR(${err.code}): ${err.message}`);
-  };
 
   self.locations = MapService.locations;
+  self.pInfowindow = self.locations.list[0]
+
+  function setTimer() {
+    console.log('set timer function');
+    if (typeof timer !== 'undefined') {
+      console.log('timer if hit');
+      clearInterval(timer)
+    }
+
+    timer = setInterval(function () {
+      self.getLocation()
+      console.log('timer started');
+      test++;
+      console.log(test);
+    }, refreshrate);
+
+  } // end setTimer
+
+  function error(err) {
+    console.log('getCurrentPosition error hit');
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    self.getLocation();
+  }; // end error function used by navigator.geolocation.getCurrentPosition
 
   // self.map = {};
   NgMap.getMap("map").then(function (map) {
     // console.log('this is  map', map);
     // console.log('this is self.map', self.map);
     self.map = map;
-  });
-
-  self.pInfowindow = self.locations.list[0]
-
-
+  }); // end NgMap.getMap
 
   self.showDetails = function (e, pin) {
-
-    // console.log('self.locations.list', self.locations.list);
-    // console.log('pin', pin);
-    // console.log('self.map is ', self.map);
+    console.log('pin clicked, showDetails function');
+    // runs when pin is clicked, displays infoWindow 
     self.pInfowindow = pin;
     var now = new Date();
     self.pInfowindow.now = now.getTime();
+    // calculate minutes and seconds
     self.pInfowindow.diff = (self.pInfowindow.now - self.pInfowindow.timestamp)
-
     self.pInfowindow.ms = self.pInfowindow.diff % 1000;
     self.pInfowindow.diff = (self.pInfowindow.diff - self.pInfowindow.ms) / 1000;
     self.pInfowindow.seconds = (self.pInfowindow.diff % 60).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
     self.pInfowindow.diff = (self.pInfowindow.diff - self.pInfowindow.seconds) / 60;
     self.pInfowindow.minutes = self.pInfowindow.diff % 60;
     self.pInfowindow.hours = (self.pInfowindow.diff - self.pInfowindow.minutes) / 60;
-
     self.map.showInfoWindow('infoWindow', this);
-  }
+  } // end self.showDetails
+
+  self.getLocation = function () {
+    console.log('getLocation hit');
+    function success(pos) {
+      console.log('getLocation success hit');
+      newPin.location = [pos.coords.latitude, pos.coords.longitude];
+      if (pos.coords.speed) {
+        newPin.speed = 0;
+      } else {
+        newPin.speed = (pos.coords.speed * 2.24);
+      };
+      self.updateLocation();
+      // setTimer();
+    };
+    navigator.geolocation.getCurrentPosition(success, error, options);
 
 
- 
-
-
-
-  self.drawMap = function () {
-    MapService.getLocations();
-  }
+  } // end self.getLocation
 
 
   self.addLocation = function (username, group) {
-    // console.log('addLocation hit');
-    // console.log('newPin line 49 is', newPin);
-
-    if (newPin.group != undefined) {
-      // console.log('add location if hit line 52');
-
-      self.updateLocation(newPin.username, newPin.group)
-
-    } else {
-      function success(pos) {
-        // console.log('add location else hit line 54');
-
-        // if (username) {
-        newPin.username = username;
-        // } else { newPin.username = "blank username" };
-        newPin.location = [pos.coords.latitude, pos.coords.longitude];
-        // if (group) {
-        newPin.group = group.toUpperCase();
-        // } else {
-        //   newPin.group = "blank group"
-        // };
-        newPin.speed = (pos.coords.speed * 2.24);
-        // console.log('pos is ', pos);
-        // console.log('newPin is ', newPin);}
-        var then = new Date();
-        newPin.timestamp = then.getTime();
-        self.sendObject(newPin);
-        setTimer(function () { console.log('add timer started') });
-      }
-    };
-    navigator.geolocation.getCurrentPosition(success, error, options);
+    console.log('addLocation hit');
+    newPin.username = username;
+    newPin.group = group.toUpperCase();
+    var timestamp = new Date();
+    newPin.timestamp = timestamp.getTime();
+    MapService.addPin(newPin)
+    // setTimer(function () { console.log('addLocation timer started') });
     $location.path("map");
-  }
+    setTimer();
+  } // end self.addLocation
 
   self.updateLocation = function (username, group) {
-    // console.log('updateLocation hit');
-    function success(pos) {
-      changePin.location = [pos.coords.latitude, pos.coords.longitude];
-      var then = new Date();
-      console.log('then is', then);
-      
-      changePin.timestamp = then.getTime();
-      console.log('changePin.timestamp is ', changePin.timestamp);
-      
-      console.log('changePin is', changePin);
-      self.sendUpdate(changePin);
-    };
-    navigator.geolocation.getCurrentPosition(success, error, options);
-    setTimer(function () { console.log('update timer started') });
-  }
+    console.log('updateLocation hit');
+    // function success(pos) {
+    // newPin.location = [pos.coords.latitude, pos.coords.longitude];
+    var then = new Date();
+    newPin.timestamp = then.getTime();
+    MapService.updatePin(newPin)
+    // }; // end success function
+    // setTimer();
+  } // end self.updateLocation
 
   self.deletePin = function (username, group) {
-    // console.log('deleteLocation hit');
-    clearTimeout(timer);
+    console.log('deleteLocation hit');
     MapService.deletePin();
     $location.path("");
-  }
+  } // end self.deletePin
 
-  self.sendObject = function (newPin) {
-    // console.log('sendObject hit');
-    MapService.addPin(newPin)
-  }
+  // self.launchApp = function () {
+    
+  //   if (launched !== 1) {
+  //     console.log('launch', launched);
+  //     self.getLocation();
+  //     launched = 1
+  //   };
+  //   launched++;
+  // }
 
-  self.sendUpdate = function (changePin) {
-    // console.log('sendUpdate hit');
-    MapService.updatePin(changePin)
-  }
+  // self.launchApp();
 
-
+  self.getLocation();
 }]);
